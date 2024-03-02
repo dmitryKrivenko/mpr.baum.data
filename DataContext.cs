c1caf6a493569ad904673f1dc7936e76c0e4370e
vtb4055336@region.vtb.ru <vtb4055336@region.vtb.ru>
2024-03-02 12:42:24 +0300

    Модели сущностей

---
+++ DataContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPR.Baum.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseNpgsql();
+        }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Owner)
+                .WithOne(o => o.User)
+                .HasForeignKey<Owner>(o => o.UserId);
+
+            modelBuilder.Entity<Owner>()
+                .HasMany(o => o.Apartments)
+                .WithMany(a => a.Owners);
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MPR.Baum.Data
+{
+    public class User
+    {
+        public Owner Owner
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int UserId
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
